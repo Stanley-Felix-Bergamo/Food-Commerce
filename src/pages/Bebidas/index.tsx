f1632e01d22e18494e64bc014398471251654eb8
Stanley-Felix-Bergamo <stanley.felix.bergamo@outlook.com>
2023-10-31 12:29:30 -0300

    commit

--- src/pages/Bebidas/index.tsx
+++ src/pages/Bebidas/index.tsx
@@ -1,11 +1,43 @@
 import Head from '../../components/Head';
 import SnackTitle from '../../components/SnackTitle';
+import Snacks from '../../components/Snacks';
 
 const Bebidas = () => {
+  const data = [
+    {
+      id: 1,
+      snack: 'Bebidas',
+      name: 'Coca-Cola 2L',
+      description: 'Coca',
+      price: 12,
+      image:
+        'https://images.pexels.com/photos/1904262/pexels-photo-1904262.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    },
+    {
+      id: 2,
+      snack: 'Bebidas',
+      name: 'Guaraná Antártica',
+      description: 'Guaraná',
+      price: 6.5,
+      image:
+        'https://oportorestaurante.com.br/site2/wp-content/webp-express/webp-images/uploads/2022/04/guarana-antarctica.jpg.webp',
+    },
+    {
+      id: 3,
+      snack: 'Bebidas',
+      name: 'Suco de laranja',
+      description: 'Suco',
+      price: 6.5,
+      image:
+        'https://images.pexels.com/photos/96974/pexels-photo-96974.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    },
+  ];
+
   return (
     <>
       <Head title='Bebidas' description='Nossos melhores Bebidas' />
       <SnackTitle>Bebidas</SnackTitle>
+      <Snacks snacks={data}></Snacks>
     </>
   );
 };
--- src/pages/Pizzas/index.tsx
+++ src/pages/Pizzas/index.tsx
@@ -1,11 +1,42 @@
 import Head from '../../components/Head';
 import SnackTitle from '../../components/SnackTitle';
+import Snacks from '../../components/Snacks';
 
 const Pizzas = () => {
+  const data = [
+    {
+      id: 1,
+      snack: 'Pizzas',
+      name: 'Bella',
+      description: 'Pizza do Cheff',
+      price: 69.9,
+      image: 'https://images.pexels.com/photos/1146760/pexels-photo-1146760.jpeg',
+    },
+    {
+      id: 2,
+      snack: 'Italiana',
+      name: 'Pizza tradicional',
+      description: 'Pizza Italiana',
+      price: 68.5,
+      image:
+        'https://images.pexels.com/photos/5593192/pexels-photo-5593192.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    },
+    {
+      id: 3,
+      snack: 'Pizzas',
+      name: 'Lombo canadense',
+      description: 'Pizza canadense',
+      price: 78.2,
+      image:
+        'https://images.pexels.com/photos/10875297/pexels-photo-10875297.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    },
+  ];
+
   return (
     <>
       <Head title='Pizzas' description='Nossos melhores Pizzas' />
       <SnackTitle>Pizzas</SnackTitle>
+      <Snacks snacks={data}></Snacks>
     </>
   );
 };
