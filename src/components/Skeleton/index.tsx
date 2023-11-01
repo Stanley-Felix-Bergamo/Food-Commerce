6ea74d9523126be441a361d57ee35babee07daaf
Stanley-Felix-Bergamo <stanley.felix.bergamo@outlook.com>
2023-11-01 16:26:26 -0300

    commit

---
+++ src/components/Skeleton/index.tsx
@@ -0,0 +1,10 @@
+import { SkeletonElement } from './styles';
+export interface SkeletonProps {
+  type: 'title' | 'text' | 'image' | 'thumbnail';
+}
+
+const Skeleton = ({ type }: SkeletonProps) => {
+  return <SkeletonElement type={type} />;
+};
+
+export default Skeleton;
--- src/components/Snacks/index.tsx
+++ src/components/Snacks/index.tsx
@@ -1,6 +1,7 @@
 import { FiPlus } from 'react-icons/fi';
 import { Container } from './styles';
 import { currecyFormat } from '../../helpers/currecyFormat';
+import SkeletonSnack from '../SkeletonSnack';
 
 interface SnacksProps {
   snacks: any[];
@@ -9,19 +10,21 @@ interface SnacksProps {
 const Snacks = ({ snacks }: SnacksProps) => {
   return (
     <Container>
-      {snacks.map((snack) => (
-        <div key={snack.id} className='snack'>
-          <h2>{snack.name}</h2>
-          <img src={snack.image} alt={snack.name} />
-          <p>{snack.description}</p>
-          <div>
-            <strong>{currecyFormat(snack.price)}</strong>
-            <button type='button'>
-              <FiPlus />
-            </button>
-          </div>
-        </div>
-      ))}
+      {!snacks.length
+        ? [1, 2, 3, 4].map((n) => <SkeletonSnack key={n} />)
+        : snacks.map((snack) => (
+            <div key={snack.id} className='snack'>
+              <h2>{snack.name}</h2>
+              <img src={snack.image} alt={snack.name} />
+              <p>{snack.description}</p>
+              <div>
+                <strong>{currecyFormat(snack.price)}</strong>
+                <button type='button'>
+                  <FiPlus />
+                </button>
+              </div>
+            </div>
+          ))}
     </Container>
   );
 };
